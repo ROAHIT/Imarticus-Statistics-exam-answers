bbd4536601ca2f24433f4fcc78a0619c45d4a5ba
ROAHIT <78689041+ROAHIT@users.noreply.github.com>
2022-04-19 15:51:14 +0530

    Add files via upload

---
+++ Q9.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# question 9
+
+from scipy import stats
+
+a=stats.poisson.cdf(80,76)
+
+print("probability of pizza sales exceeds 80 in a day=",a)
+
+
+# In[ ]:
+
+
+
+
